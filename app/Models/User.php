20ba22a2d9e0e43d6d27f468c018f9bfd5bf3648
vagrant <vagrant@homestead>
2019-02-16 07:50:27 +0000

    dd

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -9,10 +9,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+    protected $table = "users";
 
     /**
      * The attributes that are mass assignable.
-     *
+     * 能被正常更新的字段
      * @var array
      */
     protected $fillable = [
